c518605b0c55b77753d88ed908f9aa4eada63e83
Alyfer Pedroso <alyferleandroaraujopedroso@gmail.com>
2025-05-10 15:26:43 -0300

    Update header.tsx

--- src/components/template/header.tsx
+++ src/components/template/header.tsx
@@ -1,10 +1,13 @@
 import type { FC } from "react";
+import { Link } from "react-router-dom";
 
 export const Header: FC = () => {
   return (
     <header className="w-full py-4 bg-blue-20">
       <div className="w-full max-w-[1188px] mx-auto px-6">
-        <h1 className="text-white font-medium text-[1.1rem]">Alyfer Leandro de Araújo Pedroso</h1>
+        <Link to="/?radio=tracked">
+          <h1 className="text-white font-medium text-[1.1rem]">Alyfer Leandro de Araújo Pedroso</h1>
+        </Link>
       </div>
     </header>
   );
